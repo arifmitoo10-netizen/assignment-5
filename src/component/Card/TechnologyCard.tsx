aef1a3aedddaaada6e8f48ab24c39c5826c592f4
arifmitoo10-netizen <arif.mitoo10@gmail.com>
2026-09-11 23:37:41 +0600

    my 3rd commit

--- src/component/Card/TechnologyCard.tsx
+++ src/component/Card/TechnologyCard.tsx
@@ -1,23 +1,49 @@
-import React, { use } from 'react';
-import type { Technology } from '../../Type/types';
-
+import { use, useState } from "react";
+import type { Technology } from "../../Type/types";
 
 interface TechnologyCardProps {
-    TechnologyCardPromise: Promise<Technology[]>
+  TechnologyCardPromise: Promise<Technology[]>;
 }
 
-const TechnologyCard = ({TechnologyCardPromise}:TechnologyCardProps) => {
-    const TechnologyCard = use(TechnologyCardPromise);
-    console.log(TechnologyCard);
+const TechnologyCard = ({
+  TechnologyCardPromise,
+}: TechnologyCardProps) => {
 
-    return (
-         <div>
+  const technologies = use(TechnologyCardPromise);
 
-           
+  const [selectedTechs, setSelectedTechs] = useState<Technology[]>([]);
 
-    </div>
+ const handleAddToStack = (technology: Technology) => {
+  setSelectedTechs((currentStack) => {
 
-  );
+    const alreadyExists = currentStack.some(
+      (tech) => tech.id === technology.id
+    );
+
+    if (alreadyExists) {
+      return currentStack.filter(
+        (tech) => tech.id !== technology.id
+      );
+    }
+    return [...currentStack, technology];
+  });
+};
+ 
+  const handleRemove = (id: string) => {
+    setSelectedTechs((currentStack) =>
+      currentStack.filter((tech) => tech.id !== id)
+    );
+  };
+
+  const handleRemoveAll = () => {
+    setSelectedTechs([]);
+  };
+   return (
+
+    
+   )
+
+  
 };
 
 export default TechnologyCard;
